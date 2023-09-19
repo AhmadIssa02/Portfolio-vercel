df73bcd5024f30025039abc665d97d7bb0063931
ahmad <issa.ahmad2002@gmail.com>
2023-09-19 04:32:01 -0400

    fix images

--- components/ContactComponent.tsx
+++ components/ContactComponent.tsx
@@ -2,29 +2,25 @@
 // MyComponent.jsx
 import React from 'react';
 import myImage from 'public/images/Ahmad.png';
-import email from 'public/images/email.png';
-import linkedin from 'public/images/linkedin.png';
-import github from 'public/images/github.png';
-import resume from 'public/images/resume.png';
 
 function HomeComponent() {
   return (
     <div className='flex'>
         <div className=' text-[24px] font-extrabold justify-between align-middle content-center'>
             <div className='flex ml-24 mr-10 mt-20 '>
-                <img className="w-10 h-10" src={email.src} alt="email" />
+                <img className="w-10 h-10" src='/images/email.png' alt="email" />
                 <a className='ml-4 text-sky-700 hover:underline' href= "mailto:ahmad.issa.yes@gmail.com">ahmad.issa.yes@gmail.com </a>
             </div>
             <div className='flex ml-24 mr-10 mt-6 '>
-                <img className="w-10 h-10" src={linkedin.src} alt="linkedin" />
+                <img className="w-10 h-10" src='images/linkedin.png' alt="linkedin" />
                 <a className='ml-4  text-sky-700 hover:underline' href= "https://www.linkedin.com/in/ahmad-issa-02l/">https://www.linkedin.com/in/ahmad-issa-02l/ </a>
             </div>
             <div className='flex ml-24 mr-10 mt-6 '>
-                <img className="w-10 h-10" src={github.src} alt="github" />
+                <img className="w-10 h-10" src='images/github.png' alt="github" />
                 <a className='ml-4  text-sky-700 hover:underline' href= "https://github.com/AhmadIssa02">https://github.com/AhmadIssa02 </a>
             </div>
             <div className='flex ml-24 mr-10 mt-6 '>
-                <img className="w-10 h-10" src={resume.src} alt="resume" />
+                <img className="w-10 h-10" src='images/resume.png' alt="resume" />
                 <a className='ml-4  text-sky-700 hover:underline' href="/resume.pdf" download> Download Resume </a>
             </div>
         </div>
