4f64c1af6bac8cd8dc59e9c5cd15acbcb24d8a32
ahmad <issa.ahmad2002@gmail.com>
2023-09-19 04:25:01 -0400

    fix libraries

--- components/HomeComponent.tsx
+++ components/HomeComponent.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import myImage from 'public/images/Ahmad.png';
-import TextAnimation from './typewriterAnimation';
+// import TextAnimation from './typewriterAnimation';
 
 function HomeComponent() {
   const animatedText = "Software Engineer, UI/UX Designer, and a Tech Enthusiast"; // Define the text to animate
@@ -12,7 +12,8 @@ function HomeComponent() {
         <div className="ml-10 font-semibold mt-6 text-2xl text-indigo-900">
           <div className='ml-6 font-bold text-[20px]'>Hello World! </div>
           <div className='ml-6 my-2 font-bold text-5xl'>I am Ahmad Issa</div>
-          <div className='ml-6'> <TextAnimation  textToAnimate={animatedText} /> </div> 
+          <div className='ml-6'>  Software Engineer </div> 
+          {/* <TextAnimation  textToAnimate={animatedText} /> */}
           <div className="ml-6 mt-4 font-medium text-base">
             I am a software developer from Jordan, with a passion for designing and building web applications.
             <div className="mt-2"/>
--- components/typewriterAnimation.tsx
+++ components/typewriterAnimation.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import styled from 'styled-components';
-import Typist from 'react-typist';
+// import React from 'react';
+// // import styled from 'styled-components';
+// // import Typist from 'react-typist';
 
-interface TextAnimationProps {
-  textToAnimate: string; // Define a prop for the text to animate
-}
+// interface TextAnimationProps {
+//   textToAnimate: string; // Define a prop for the text to animate
+// }
 
-const TextAnimation: React.FC<TextAnimationProps> = ({ textToAnimate }) => {
-  return (
-    <Wrapper>
-      <Typist
-        startDelay={1000}
-        cursor={{ show: false }}
-        onTypingDone={() => console.log('Animation done')}
-      >
-        <TypedText>{textToAnimate}</TypedText>
-      </Typist>
-    </Wrapper>
-  );
-};
+// const TextAnimation: React.FC<TextAnimationProps> = ({ textToAnimate }) => {
+//   return (
+//     // <Wrapper>
+//       {/* <Typist
+//         startDelay={1000}
+//         cursor={{ show: false }}
+//         onTypingDone={() => console.log('Animation done')}
+//       >
+//         <TypedText>{textToAnimate}</TypedText>
+//       </Typist> */}
+//     {/* </Wrapper> */}
+//   );
+// };
 
-const Wrapper = styled.div`
-  font-size: 20px;
-  text-align: center;
-  margin-top: 20px;
-`;
+// // const Wrapper = styled.div`
+// //   font-size: 20px;
+// //   text-align: center;
+// //   margin-top: 20px;
+// // `;
 
-const TypedText = styled.div`
-  display: inline;
-  color: indigo-900
-`;
+// const TypedText = styled.div`
+//   display: inline;
+//   color: indigo-900
+// `;
 
-export default TextAnimation;
+// export default TextAnimation;
