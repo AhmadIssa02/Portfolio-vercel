e6536484cce4ef7d1ade310205e5145518c5bb20
Ahmad Issa <issa.ahmad2002@gmail.com>
2024-01-03 00:10:28 +0300

    fix contact page

--- components/ContactComponent.tsx
+++ components/ContactComponent.tsx
@@ -10,31 +10,32 @@ import download from 'public/images/download.png';
 
 function HomeComponent() {
   return (
-    <div className='flex'>
-        <div className='text-[12px] md:text-[18px] lg:text-[24px] font-extrabold justify-between align-middle content-center'>
-            <div className='flex ml-2 md:ml-6 lg:ml-20 mt-8 lg:mt-20 '>
+    <div className='flex mt-4'>
+        <div className='lg:ml-4 text-[11px] md:text-[18px] lg:text-[24px] font-extrabold justify-between align-middle content-center'>
+            <div className='flex ml-2 md:ml-6 lg:ml-20 mt-8 lg:mt-16 underline hover:scale-110 '>
                 <img className="w-10 h-10" src= {email.src} alt="email" />
                 <a className='ml-1 md:ml-4 mt-2 lg:mt-0 text-black hover:underline' href= "mailto:ahmad.issa.yes@gmail.com">ahmad.issa.yes@gmail.com </a>
             </div>
-            <div className='flex ml-2 md:ml-6 lg:ml-20 mt-6 '>
+            <div className='flex ml-2 md:ml-6 lg:ml-20 mt-6 underline hover:scale-110'>
                 <img className="w-10 h-10" src={linkedin.src} alt="linkedin" />
-                <a className='ml-1 md:ml-4 lg:mt-0 text-black hover:underline' href= "https://www.linkedin.com/in/ahmad-issa-02l/">https://www.linkedin.com/in/ahmad-issa-02l/ </a>
+                <a className='ml-1 md:ml-4 lg:mt-0 text-black hover:underline' href= "https://www.linkedin.com/in/ahmad-issa-02l/">Ahmad Issa </a>
             </div>
-            <div className='flex ml-2 md:ml-6 lg:ml-20 mt-6 '>
+            <div className='flex ml-2 md:ml-6 lg:ml-20 mt-6 underline hover:scale-110'>
                 <img className="w-10 h-10" src={github.src} alt="github" />
-                <a className='ml-1 md:ml-4 mt-2 lg:mt-0  text-black hover:underline' href= "https://github.com/AhmadIssa02">https://github.com/AhmadIssa02 </a>
+                <a className='ml-1 md:ml-4 mt-2 lg:mt-0  text-black hover:underline' href= "https://github.com/AhmadIssa02">AhmadIssa02 </a>
             </div>
-            <div className='flex  ml-2 md:ml-6 lg:ml-20  mt-6 '>
+            <div className='flex ml-2 md:ml-6 lg:ml-20  mt-6 underline hover:scale-110'>
                 <img className="w-12 h-12" src={resume.src} alt="resume" />
                 <a className='ml-0 md:ml-2 mt-2 lg:mt-0 text-black hover:underline' href="/resume.pdf" download>Download Resume </a>
                 <img className="mt-1 w-3 h-3 md:w-5 md:h-5" src={download.src} alt="download" />
             </div>
         </div>
-        <img className=" bottom-0 w-64 h-64 md:w-96 md:h-96 mt-6 " src={myImage.src} alt="myImage" />
+        <img className=" bottom-0 w-48 h-48 sm:w-64 sm:h-64 md:w-80 md:h-2/3 mt-auto lg:ml-16" src={myImage.src} alt="myImage" />
      
     </div>
   );
 }
 
 export default HomeComponent;
+
 
